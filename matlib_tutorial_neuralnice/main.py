2ceba4e2ace9237b0c29a54f90bf3a8e54449a0f
Rasulbek_Mashalov <rasulbekmashalov@mailbox.unideb.hu>
2026-01-23 11:30:13 +0100

    matlib tutorial, source:NeuralNine

---
+++ matlib_tutorial_neuralnice/main.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import style
+import random
+
+heads_tails = [0,0]
+
+for _ in range(1000): #->#dont run this in juypternotebook with big values RANGEs
+    heads_tails[random.randint(0,1)] +=1
+    plt.bar(["Heads", "Tails"], heads_tails, color=["red", "blue"])
+    plt.pause(0.001)
+plt.show() 
